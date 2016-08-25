728a261c081e4c8a4575edb5fa124ea65f5b1495
Harshil Darji <darjiharshil2994@gmail.com>
2016-08-25 20:45:56 +0530

    initial

---
+++ anagrams.py
@@ -0,0 +1,27 @@
+import collections, pprint, time
+
+start_time = time.time()
+print('creating word list...')
+word_list = sorted(list(set([word.strip().lower() for word in open('words')])))
+
+def signature(word):
+    return ''.join(sorted(word))
+
+word_bysig = collections.defaultdict(list)
+for word in word_list:
+    word_bysig[signature(word)].append(word)
+
+def anagram(myword):
+    return word_bysig[signature(myword)]
+
+print('finding anagrams...')
+all_anagrams = {word: anagram(word)
+                for word in word_list if len(anagram(word)) > 1}
+
+print('writing anagrams to file...')
+with open('anagrams.txt', 'w') as file:
+    file.write('all_anagrams = ')
+    file.write(pprint.pformat(all_anagrams))
+
+total_time = round(time.time() - start_time, 2)
+print('Done [', total_time, 'seconds ]')
